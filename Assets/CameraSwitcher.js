912c9522cea0dfeef6b8e238ad5b3c6206b07197
J Wilson <jbwilson1031@gmail.com>
2016-07-07 15:24:06 -0400

    7/7

---
+++ Assets/CameraSwitcher.js
@@ -0,0 +1,17 @@
+﻿ public var spawnCamera : Camera;
+ public var carCamera : Camera;
+ 
+ function Start() {
+  spawnCamera.GetComponent.<Camera>().enabled = true;
+ carCamera.GetComponent.<Camera>().enabled = false;
+ }
+ 
+ function Update() {
+ 
+ if(Input.GetKey(KeyCode.N))
+ {
+ spawnCamera.GetComponent.<Camera>().enabled = false;
+ carCamera.GetComponent.<Camera>().enabled = true;
+ 
+ }
+ }
--- Assets/DoorOpening.js
+++ Assets/DoorOpening.js
@@ -1,49 +1,28 @@
 ﻿#pragma strict
 
-private var guiShow : boolean = false;
-
-var isOpen : boolean = false;
-
-var door : GameObject;
-
-var rayLength = 10;
-
-function Update()
+function Update () 
 {
-	var hit : RaycastHit;
-	var fwd = transform.TransformDirection(Vector3.forward);
+var hit : RaycastHit;
+var ray : Ray = Camera.main.ScreenPointToRay(Vector3(Screen.width*0.5,Screen.height*0.5, 0));
+ if (Physics.Raycast (ray, hit, 3))
+ {
+    if (hit.collider.gameObject.tag == "Door")
+ {
+  if(Input.GetKeyDown("e"))
+  {
+   hit.collider.gameObject.GetComponent.<Animation>().Play("DoorrOpen");
+  hit.collider.gameObject.tag = "Close";
+  }
+ }
+    else if (hit.collider.gameObject.tag == "Close")
+ {
+  if(Input.GetKeyDown("e"))
+  {
+   hit.collider.gameObject.GetComponent.<Animation>().Play("DoorrClose");
+    hit.collider.gameObject.tag = "Door";
+  }
+ }
 	
-	if(Physics.Raycast(transform.position, fwd, hit, rayLength))
-	{
-		if(hit.collider.gameObject.tag == "Door")
-		{
-			guiShow = true;
-			if(Input.GetKeyDown("e") && isOpen == false)
-			{
-				door.GetComponent.<Animation>().Play("DoorOpen");
-				isOpen = true;
-				guiShow = false;
-			}
 			
-			else if(Input.GetKeyDown("e") && isOpen == true)
-			{
-				door.GetComponent.<Animation>().Play("DoorClose");
-				isOpen = false;
-				guiShow = false;
-			}
-		}
-	}
-	
-	else
-	{
-		guiShow = false;
-	}
-}
-
-function OnGUI()
-{
-	if(guiShow == true && isOpen == false)
-	{
-		GUI.Box(Rect(Screen.width / 2, Screen.height / 2, 100, 25), "Use Door");
-	}
-}
+ }
+ }
